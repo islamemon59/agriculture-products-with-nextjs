b8ad34ca5e781e0776f25cc1a69991b8317270cb
islamemon59 <islamemon59@gmail.com>
2025-08-01 09:04:31 +0600

    agriculute product added

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import Banner from "./Components/Banner/Banner";
+import AgricultureProduct from "./Components/AgricultureProducts/AgricultureProducts";
 
 export default function Home() {
   return (
     <div>
       <section>
         <Banner />
+      </section>
+      <section>
+        <AgricultureProduct/>
       </section>
     </div>
   );
