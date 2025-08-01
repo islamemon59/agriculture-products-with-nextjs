f650162472cc6d1bcdbe28b681cc3d0c427883da
islamemon59 <islamemon59@gmail.com>
2025-08-01 17:16:46 +0600

    next auth mongobd resgister router user data collection added

---
+++ src/lib/authOptions.js
@@ -0,0 +1,49 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import { collectionObj, dbConnect } from "./dbConnect";
+import { compare } from "bcryptjs";
+
+export const authOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const userCollection = dbConnect(collectionObj.userCollection)
+
+        const user = await userCollection.findOne({ email: credentials.email });
+
+        if (!user) {
+          throw new Error("No user found with this email");
+        }
+
+        const isValid = await compare(credentials.password, user.password);
+        if (!isValid) {
+          throw new Error("Incorrect password");
+        }
+
+        return {
+          id: user._id,
+          name: user.name,
+          email: user.email,
+        };
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user = token.user;
+      return session;
+    },
+  },
+//   secret: process.env.NEXTAUTH_SECRET,
+};
---
+++ src/lib/dbConnect.js
@@ -0,0 +1,17 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = process.env.NEXT_PUBLIC_MONGODB_URI;
+
+export const collectionObj = {
+  userCollection: "userData",
+};
+
+export const dbConnect = (collectionName) => {
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  return client.db(process.env.DB_NAME).collection(collectionName);
+};
